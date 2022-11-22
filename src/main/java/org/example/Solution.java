1a7b324a60ca0632b4e50109ac7e6c08dc64a2e3
annt <annt@gft.com>
2022-11-22 12:05:26 +0000

    Done with Hashmap, Set and streams

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,46 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.*;
+
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.toMap;
+
+public class Solution {
+
+    public Solution(){
+
+    }
+
+    public int[] KWeakestRows(int[][] mat, int k) {
+
+        int[] weakestRows;
+
+        Map<Integer,Integer> rowPower = new HashMap<>();
+
+        int power = 0;
+
+        for(int i = 0;i< mat.length;i++){
+            for (int j = 0; j<mat[i].length; j++){
+                power += mat[i][j];
+            }
+            rowPower.put(i,power);
+            power = 0;
+        }
+        Map<Integer, Integer> sortedMap = rowPower.entrySet().stream()
+                .sorted(comparingInt(Map.Entry::getValue))
+                .limit(k)
+                .collect(toMap(Map.Entry::getKey,Map.Entry::getValue,
+                        (a, b) -> { throw new AssertionError();},
+                        LinkedHashMap::new
+                ));
+
+        Set<Integer> keys = sortedMap.keySet();
+
+        weakestRows = new int[keys.size()];
+
+        int index = 0;
+        for(Integer element : keys) weakestRows[index++] = element;
+
+        return weakestRows;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class SolutionTest {
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SolutionTest {
+
+    Solution solution;
+
+    int[][] mat;
+
+    int limit;
+
+    int[] result;
+
+    @BeforeAll
+    void setup(){
+        solution = new Solution();
+
+    }
+
+    @Test
+    void matrixWithTwoRowsAndLimitOne(){
+
+        mat = new int[][]{{1, 0, 0, 0, 0}, {1, 1, 0, 0, 0}};
+
+        limit = 1;
+
+        result = new int[] {0};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.KWeakestRows(mat, limit)));
+    }
+
+    @Test
+    void matrixWithFiveRowsAndLimitThree(){
+
+        mat = new int[][]{{1, 0, 0, 0, 0}, {1, 1, 0, 0, 0},{0, 0, 0, 0, 0},{1, 1, 0, 0, 0},{1, 1, 1, 1, 0}};
+
+        limit = 3;
+
+        result = new int[] {2,0,1};
+
+        assertEquals(Arrays.toString(result), Arrays.toString(solution.KWeakestRows(mat, limit)));
+    }
 }
